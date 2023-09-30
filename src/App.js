c3f013bfe2fc84251f481646e7208cb7efcf0e5d
Hashini <hashikavi1@gmail.com>
2023-10-01 00:31:35 +0530

    first comment

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div >
+      <h2>Let's get start</h2>
+      <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,5 @@
+function ExpenseItem(){
+    return <h1>First item</h1>
+}
+
+export default ExpenseItem;
